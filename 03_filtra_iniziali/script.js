c33e8d02661ff911305419ed06dcb5119b085591
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-09 17:17:59 +0100

    Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera

--- 03_filtra_iniziali/script.js
+++ 03_filtra_iniziali/script.js
@@ -1,13 +1,25 @@
-/* Scrivi una funzione che accetti un'array di stringhe e una lettera e restituisca un array contenente solo le parole che iniziano con quella lettera */
-
+/* Scrivi una funzione che accetti un'array di stringhe e una lettera e 
+restituisca un array contenente solo le parole che iniziano con quella lettera */
 const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 
 // Dichiara la funzione qui.
+function getWordsStartingWith (array, letter) {
+    let  wordsStartingWith = [];
+    let j=0;
 
+    for (let i=0; i<array.length; i++){
+        if ( array[i][0] === letter){
+            wordsStartingWith[j] = array[i];
+            j++;
+        }
+    }
+    return  wordsStartingWith;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+const letter = prompt(`Inserire una consonante:`);
+console.log(getWordsStartingWith(names, letter));
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
