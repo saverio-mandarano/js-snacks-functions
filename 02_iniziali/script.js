544de8709c0a713f497d9edd3c0b499a8441092d
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-09 17:04:28 +0100

    Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -1,13 +1,19 @@
-/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
-
+/* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le 
+iniziali di ogni parola dell'array fornito */
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 
 // Dichiara la funzione qui.
-
+function getFirstInitial (array) {
+    let initialNames = [];
+    for (let i=0; i<array.length; i++){
+        initialNames[i] = array[i][0];
+    }
+    return initialNames;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(getFirstInitial (names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
