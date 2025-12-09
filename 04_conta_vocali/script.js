2f7bcbb5bed34e632f33ce9837ce93464096b4de
saverio mandarano <saverio.mandarano1@gmail.com>
2025-12-09 17:41:33 +0100

    Scrivi una funzione che accetti una stringa  e restituisca il numero di vocali contenute al suo interno

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -1,10 +1,29 @@
-/* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
-
+/* Scrivi una funzione che accetti una stringa
+ e restituisca il numero di vocali contenute al suo interno */
 const word = 'javascript';
 
 
 // Dichiara la funzione qui.
+function getNumberOfVowels (string){
+    let numberOfVowels = 0;
+    const vowels = [
+        `a`,
+        `e`,
+        `i`,
+        `o`,
+        `u`
+    ];
 
+    for (let i=0; i<string.length; i++){
+        for (let j=0; j<vowels.length; j++){
+            if ( string[i].toLowerCase() === vowels[j] ){
+                numberOfVowels ++;
+            }
+        }
+    }
+
+    return numberOfVowels;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
